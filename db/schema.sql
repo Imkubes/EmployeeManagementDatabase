894a3db43ece78106d8efdb2ca311700dd7cc922
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-04-26 23:25:07 -0500

    stuck

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS employeeManager_db;
+-- Creates the "employee_db" database --
+CREATE DATABASE employeeManager_db;
+-- Use "employeeManager_db" so that the following code effects "employeeManager_db" --
+USE employeeManager_db;
+
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT,
+    department_name VARCHAR(35),
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    job_title VARCHAR(30),
+    salary DECIMAL (10,2),
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments (id)
+    ON DELETE CASCADE
+);
+
+-- Create table allEmployees inside of "employeeManager_db" --
+CREATE TABLE allemployees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles (id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES allemployees (id)
+    ON DELETE CASCADE
+);
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+USE employeeManager_db;
+
+INSERT INTO departments (department_name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO roles (job_title,salary,department_id)
+VALUES
+    ('Sales Lead', 50000,1),
+    ('Salesman', 45000,1),
+    ('Lead Engineer', 75000,2),
+    ('Software Engineer', 65000,2),
+    ('Account Manager', 50000,3),
+    ('Lawyer', 200000,4)
+
+
+INSERT INTO allemployees (first_name,last_name,role_id,manager_id)
+VALUES
+    ('Bob','Swaggerty',3,2),
+    ('Wilson','Zero',4,1),
+    ('John','Doe',2,3),
+    ('James','Bond',4,1);
